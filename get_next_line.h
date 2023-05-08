09d157839d7afac5f4bdee56689f985cc208dc0a
Gabriela <krustagabriela@gmail.com>
2023-05-08 13:49:34 +0200

    ft_find_char done

--- get_next_line.h
+++ get_next_line.h
@@ -6,12 +6,16 @@
 /*   By: gkrusta <gkrusta@student.42malaga.com>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/07 19:00:34 by gkrusta           #+#    #+#             */
-/*   Updated: 2023/05/07 19:08:59 by gkrusta          ###   ########.fr       */
+/*   Updated: 2023/05/08 12:33:25 by gkrusta          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 5
+# endif
 
 # include <stdlib.h>
 
